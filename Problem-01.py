074e11413d8d657987eb8ade66908d1bdeb05d17
Arindam Dey <deyarindam475@gmail.com>
2021-07-16 20:28:45 +0530

    Create Problem-01.py

---
+++ Problem-01.py
@@ -0,0 +1,23 @@
+Python Program to Print Hello world!
+
+
+
+Source Code
+
+# This program prints Hello, world!
+
+print('Hello, world!')
+
+
+Output
+
+Hello, world!
+
+
+
+
+
+
+In this program, we have used the built-in print() function to print the string Hello, world! on our screen.
+
+By the way, a string is a sequence of characters. In Python, strings are enclosed inside single quotes, double quotes, or triple quotes.
