81fa500cf4b357c5a6de81bfaf177727bee5f5de
Arindam Dey <deyarindam475@gmail.com>
2021-07-19 21:33:57 +0530

    Create Problem-09.py

---
+++ Problem-09.py
@@ -0,0 +1,21 @@
+Python Program to Print Output Without a Newline
+
+
+# print each statement on a new line
+print("Python")
+print("is easy to learn.")
+
+# new line
+print()
+
+# print both the statements on a single line
+print("Python", end=" ")
+print("is easy to learn.")
+
+
+Output
+
+Python
+is easy to learn.
+
+Python is easy to learn.
