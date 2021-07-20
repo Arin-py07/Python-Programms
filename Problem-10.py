08a3d1b83268fa3e2e71ee32327141c5f0840e5b
Arindam Dey <deyarindam475@gmail.com>
2021-07-20 23:54:34 +0530

    Create Problem-10.py

---
+++ Problem-10.py
@@ -0,0 +1,34 @@
+Python Program to Differentiate Between type() and isinstance()
+
+
+Difference between type() and instance()
+
+Let's understand the difference between type() and instance() with the example code below.
+
+class Polygon:
+    def sides_no(self):
+        pass
+
+class Triangle(Polygon):
+    def area(self):
+        pass
+
+obj_polygon = Polygon()
+obj_triangle = Triangle()
+
+print(type(obj_triangle) == Triangle)   	# true
+print(type(obj_triangle) == Polygon)    	# false
+
+print(isinstance(obj_polygon, Polygon)) 	# true
+print(isinstance(obj_triangle, Polygon))	# true
+
+
+Output
+
+True
+False
+True
+True
+
+
+In the above example, we see that type() cannot distinguish whether an instance of a class is somehow related to the base class. In our case, although obj_triangle is an instance of child class Triangle, it is inherited from the base class Polygon. If you want to relate the object of a child class with the base class, you can achieve this with instance().
