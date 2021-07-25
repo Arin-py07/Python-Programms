7ceb02a1f54077757bd9376da9bce8d8a5cd0be8
Arindam Dey <deyarindam475@gmail.com>
2021-07-25 23:52:23 +0530

    Update Problem-12.py

--- Problem-12.py
+++ Problem-12.py
@@ -7,7 +7,7 @@ Source Code
 
 # Python program to check if the input number is odd or even.
 # A number is even if division by 2 gives a remainder of 0.
-# If the remainder is 1, it is an odd number....
+# If the remainder is 1, it is an odd number.......
 
 num = int(input("Enter a number: "))
 if (num % 2) == 0:
